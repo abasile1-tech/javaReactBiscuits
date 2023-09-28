317c9346d0055da1e4cb0cd49d843469286e04f5
John <ashton.basile@gmail.com>
2023-09-28 14:30:52 +0100

    first commit

---
+++ javaBiscuitsAPI/src/main/java/com/example/javaBiscuitsAPI/JavaBiscuitsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaBiscuitsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBiscuitsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBiscuitsApiApplication.class, args);
+	}
+
+}
